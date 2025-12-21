db3c40cb126f43e7cd7e0aa98636052c4a1bd3fb
prathamcodecody-code <pratham.codecody@gmail.com>
2025-12-21 14:18:49 +0530

    Update SearchClient.tsx

--- app/search/SearchClient.tsx
+++ app/search/SearchClient.tsx
@@ -95,7 +95,9 @@ export default function SearchClient() {
 
       <div className="grid grid-cols-12 gap-6">
         <aside className="hidden md:block md:col-span-3">
-          <FiltersSidebar />
+          {categoryId && (
+  <FiltersSidebar categoryId={categoryId as string} />
+)}
         </aside>
 
         <main className="col-span-12 md:col-span-9">
