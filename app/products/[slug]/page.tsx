976ebc5a784d442438789f5c60b255ad4fa66dfd
prathamcodecody-code <pratham.codecody@gmail.com>
2025-12-21 16:23:35 +0530

    Update page.tsx

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -8,16 +8,9 @@ type PageProps = {
   params: { slug: string };
 };
 
-/* ❌ DO NOT FETCH API IN METADATA */
-export async function generateMetadata() {
-  return {
-    title: "Product | FirstFemale",
-  };
-}
-
 export default async function ProductPage({ params }: PageProps) {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/products/${params.slug}`,
+    `${process.env.API_URL}/products/${params.slug}`,
     { cache: "no-store" }
   );
 
